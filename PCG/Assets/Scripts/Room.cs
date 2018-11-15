4ec7ae489a838dcbe22ddc9afc3efd883f0648a3
Bruno Couto <couto-10@hotmail.com>
2018-11-15 10:09:46 +0000

    Update
    Project added... without being on a rar file

---
+++ PCG/Assets/Scripts/Room.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour {
+    
+    public int Id { get; private set; }
+    public bool IsMainRoom { get; private set; }
+    public bool IsVisible { get; private set; }
+    public bool IsLocked { get; private set; }
+    public bool IsSleeping { get { return RigidBody2D.IsSleeping(); } }
+    public bool IsStartRoom { get; private set; }
+    public bool IsEndRoom { get; private set; }
+    public Vector3 Center { get { return transform.position; } }
+    public Vector3 TopLeft { get { return new Vector3(transform.position.x - transform.localScale.x / 2f, transform.position.y + transform.localScale.y / 2f); } }
+    public Vector3 BottomRight { get { return new Vector3(transform.position.x + transform.localScale.x / 2f, transform.position.y - transform.localScale.y / 2f); } }
+    public Color SecondaryColor = new Color(0.8f, 0.8f, 0.8f);
+    public Color MainColor = new Color(200 / 255f, 150 / 255f, 65 / 255f);
+    public Color DisabledColor = new Color(0.2f, 0.2f, 0.2f, 0.5f);
+    public Color StartRoomColor = new Color(15 / 255f, 15 / 255f, 195 / 255f);
+    public Color EndRoomColor = new Color(195 / 255f, 15 / 255f, 15 / 255f);
+    
+    SpriteRenderer Background;
+    Rigidbody2D RigidBody2D;
+    
+    public List<RoomConnection> Connections { get; private set; }
+    
+    void Awake()
+    {
+        Background = GetComponent<SpriteRenderer>();
+        RigidBody2D = GetComponent<Rigidbody2D>();
+
+        Connections = new List<RoomConnection>();
+
+        IsVisible = true;
+    }
+    
+    public void Init(int id, Vector2 position, int width, int height)
+    {
+        Id = id;
+
+        transform.position = position;
+        transform.localScale = new Vector2(width, height);
+    }
+    
+    public void SetMain()
+    {
+        IsMainRoom = true;
+    }
+    
+    public void SetLocked(bool locked)
+    {
+        IsLocked = locked;
+    }
+    
+    public void SetVisible(bool visible)
+    {
+        IsVisible = visible;
+    }
+    
+    public void SetStartRoom()
+    {
+        IsStartRoom = true;
+    }
+    
+    public void SetEndRoom()
+    {
+        IsEndRoom = true;
+    }
+    
+    public void AddRoomConnection(RoomConnection connection)
+    {
+        if (!Connections.Contains(connection))
+        {
+            Connections.Add(connection);
+        }
+    }
+    
+    public void Snap()
+    {
+        int x = Mathf.CeilToInt(transform.position.x);
+        int y = Mathf.FloorToInt(transform.position.y);
+
+        transform.position = new Vector2(x, y);
+    }
+    
+    void FixedUpdate()
+    {
+        if (!IsLocked)
+        {
+            Snap();
+        }
+    }
+    
+    void Update()
+    {
+        if (IsVisible)
+        {
+            if (IsMainRoom)
+            {
+                if (IsStartRoom)
+                    Background.color = StartRoomColor;
+                else if (IsEndRoom)
+                    Background.color = EndRoomColor;
+                else
+                    Background.color = MainColor;
+            }
+            else
+                Background.color = SecondaryColor;
+        }
+        else
+        {
+            Background.color = DisabledColor;
+        }
+    }
+}
